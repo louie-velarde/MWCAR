b3255a1a8c9056dde5b5a74b174dbb6452480049
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2025-07-10 12:08:08 +0800

    Update icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "me.velc.mwcar"
         minSdk = 19
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         base.archivesName = "MWCAR-$versionName"
     }
